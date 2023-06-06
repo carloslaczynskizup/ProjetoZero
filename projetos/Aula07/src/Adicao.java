9b15732112f4c756ae5c00dd71a0e4b6ce3cde36
carloslaczynskizup <carlos.laczynski@zup.com.br>
2023-06-06 16:02:17 -0300

    Projetos - Atualizando...

--- projetos/Aula07/src/Adicao.java
+++
@@ -1,8 +0,0 @@
-public class Adicao {
-    public static void main(String[] args) {
-        int numero1 = 5;
-        int numero2 = 10;
-
-        System.out.println("O resultado é: " + (numero1 + numero2));
-    }
-}
