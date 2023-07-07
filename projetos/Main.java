9efaf5465890de254a6b0edda05f889c184e1d81
carloslaczynskizup <carlos.laczynski@zup.com.br>
2023-07-07 15:38:59 -0300

    UP Recomendação Joy

--- projetos/Main.java
+++ projetos/Main.java
@@ -6,6 +6,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
+
         Locale.setDefault(Locale.US);
 
         Scanner scanner = new Scanner(System.in);
